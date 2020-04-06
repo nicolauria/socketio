55b7b37bfbc5976edf2a647defd992011504ac57
Nico Lauria <nicolauria@outlook.com>
2020-04-05 20:44:59 -0700

    fix port issue

--- server.js
+++ server.js
@@ -42,6 +42,8 @@ io.on("connection", function (socket) {
   });
 });
 
-http.listen(3000, function () {
+const port = process.env.PORT || 3000;
+
+http.listen(port, function () {
   console.log("listening on *:3000");
 });
